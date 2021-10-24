75993ae27def9e3cf5f271a7f24b4ece4370c5e8
SaiShashank-zelar <shashank@zelarsoft.com>
2021-10-24 13:35:21 +0530

    updated

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -5,49 +5,68 @@ terraform {
       version = "=2.46.0"
     }
   }
-  backend "azurerm" {
-        resource_group_name  = "sample-vm"
-        storage_account_name = "shashanktfstate1234"
-        container_name       = "terra"
-        key                  = "terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
   features {}
 }
 
+data "azurerm_resource_group" "main" {
+  name     = "sample-vm"
+}
 
-resource "azurerm_virtual_machine" "main" {
-  name                  = "ubuntu-vm"
-  location              = azurerm_resource_group.main.location
-  resource_group_name   = azurerm_resource_group.main.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_DS1_v2"
+resource "azurerm_virtual_network" "main" {
+  name                = "azure-network"
+  allocation_method   = "Dynamic"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+}
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  allocation_method    = "Dynamic"
+}
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = data.azurerm_resource_group.main.location
+    resource_group_name          = data.azurerm_resource_group.main.name
+    allocation_method            = "Dynamic"
+}
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+resource "azurerm_network_interface" "main" {
+  name                = "azure-nic"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
 
-  storage_image_reference {
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                            = "azure-vm"
+  resource_group_name             = data.azurerm_resource_group.main.name
+  location                        = data.azurerm_resource_group.main.location
+  network_interface_ids           = [azurerm_network_interface.main.id]
+  size                            = "Standard_D2s_v3"
+  admin_username                  = "nodesample"
+  admin_password                  = "Shashank12345"
+  disable_password_authentication = false
+
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
   }
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+}
