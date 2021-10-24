e6897adc3d2cfd8cabde08bc05b7ab16290b9d99
SaiShashank-zelar <shashank@zelarsoft.com>
2021-10-24 14:32:38 +0530

    updated

--- virtualmachine/main.tf
+++ virtualmachine/main.tf
@@ -13,11 +13,12 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "main" {
   name     = "sample-vm"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "azure-network"
-  allocation_method   = "Dynamic"
+  address_space       = ["10.0.0.0/22"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
@@ -26,7 +27,7 @@ resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  allocation_method    = "Dynamic"
+  address_prefixes     = ["10.0.2.0/24"]
 }
 resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
